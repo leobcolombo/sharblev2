db527cd8b2f7ece7be438b50d2900c5bca963498
Leonardo <leonardo@academiadaweb.com.br>
2016-06-06 17:32:53 -0300

    list items

--- app/Http/Controllers/Api/Client/ClientCheckoutController.php
+++ app/Http/Controllers/Api/Client/ClientCheckoutController.php
@@ -49,7 +49,7 @@ class ClientCheckoutController extends Controller
     {
         $id = Authorizer::getResourceOwnerId();
         $clientId = $this->userRepository->find($id)->client->id;
-        $orders = $this->orderRepository->scopeQuery(function ($query) use ($clientId) {
+        $orders = $this->orderRepository->with(['items'])->scopeQuery(function ($query) use ($clientId) {
             return $query->where('client_id', '=', $clientId);
         })->paginate();
 
